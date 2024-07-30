62c1eaf4638353c968f2c97b95ab3ba48cf9f462
moriyama07ichino <chikano.kato.07@gmail.com>
2024-07-30 20:46:59 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,8 @@ class ItemsController < ApplicationController
   def index  # indexアクションを定義した
     @item = "これはコントローラーで定義したインスタンス変数を確認するための文字列です"
   end
+
+  def item_params
+    params.require(:item).permit(:name, :description, :image) # 既存のパラメータ名に:imageを追加
+  end
 end

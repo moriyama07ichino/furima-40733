c5f8bd6e4a513c9b3df58ce31fb7f174bc92af38
moriyama07ichino <chikano.kato.07@gmail.com>
2024-08-01 22:46:16 +0900

    手順5 商品出品ページを表示できるようにする

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,24 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]  # newとcreateアクションに適用
+
   def index  # indexアクションを定義した
     @item = "これはコントローラーで定義したインスタンス変数を確認するための文字列です"
   end
+
+  def new
+    @item = Item.new  # 新しい商品のための空のインスタンスを作成
+  end
+
+  def create
+    @item = Item.new(item_params)  # フォームから送信されたデータで新しい商品を作成
+    if @item.save
+      redirect_to @item  # 保存が成功したら詳細ページにリダイレクト
+    else
+      render :new  # 保存が失敗したらnewテンプレートを再表示
+    end
+  end
+
+  private
 
   def item_params
     params.require(:item).permit(:name, :description, :image) # 既存のパラメータ名に:imageを追加

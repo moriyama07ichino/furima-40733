d48185e951acc587f512f55103165c0fda80de4d
moriyama07ichino <chikano.kato.07@gmail.com>
2024-08-04 00:43:04 +0900

    手順2 必要な要素を取得する

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -4,3 +4,14 @@ document.addEventListener('DOMContentLoaded', () => {
 window.addEventListener('turbo:load', () => {
   console.log("OK");
 });
+
+// 1. 要素の取得
+const priceInput = document.getElementById("item-price");
+console.log(priceInput);
+
+const feeDisplay = document.getElementById("add-tax-price");
+console.log(feeDisplay);
+
+const profitDisplay = document.getElementById("profit");
+console.log(profitDisplay);
+

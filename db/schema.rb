eb1de152de7eadb780a8414cb7a285b67bf252b4
moriyama07ichino <chikano.kato.07@gmail.com>
2024-08-10 21:05:44 +0900

    修正５

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_08_03_125911) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_30_113536) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -39,21 +39,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_08_03_125911) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "articles", charset: "utf8", force: :cascade do |t|
-    t.bigint "user_id"
-    t.string "name", null: false
-    t.text "description", null: false
-    t.integer "category_id", null: false
-    t.integer "condition_id", null: false
-    t.integer "shipping_cost_id", null: false
-    t.integer "prefecture_id", null: false
-    t.integer "days_to_ship_id", null: false
-    t.integer "price", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_articles_on_user_id"
-  end
-
   create_table "items", charset: "utf8", force: :cascade do |t|
     t.bigint "user_id"
     t.string "name", null: false
@@ -67,15 +52,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_08_03_125911) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_items_on_user_id"
-  end
-
-  create_table "orders", charset: "utf8", force: :cascade do |t|
-    t.bigint "item_id", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["item_id"], name: "index_orders_on_item_id"
-    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "users", charset: "utf8", force: :cascade do |t|
@@ -98,8 +74,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_08_03_125911) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "articles", "users"
   add_foreign_key "items", "users"
-  add_foreign_key "orders", "items"
-  add_foreign_key "orders", "users"
 end
